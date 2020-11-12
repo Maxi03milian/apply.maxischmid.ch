292f11e0f1e2f0940a7a41b7e9c00f7317cfc700
Maxi03milian <46729527+Maxi03milian@users.noreply.github.com>
2020-11-12 13:50:15 +0100

    Code transfer ük aufg 9 to apply

--- public/main.js
+++ public/main.js
@@ -1,33 +1,16 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyCIrnk0bubF5kZ7DH7seoNBJeHSsSISrzk",
-  authDomain: "apply-maxischmid.firebaseapp.com",
-  databaseURL: "https://apply-maxischmid.firebaseio.com",
-  projectId: "apply-maxischmid",
-  storageBucket: "apply-maxischmid.appspot.com",
-  messagingSenderId: "489119852536",
-  appId: "1:489119852536:web:7456b9b9609d306fd7d80d",
-  measurementId: "G-XZ16ZJJ9MV"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-const firestore = firebase.firestore();
-const colApplications = firestore.collection('applications');
+AOS.init()
 
 
-//Cache elements
-const welcome = document.getElementById('welcome');
+new TypeIt("#element", {
+  speed: 75,
+  loop: true
+})
+  .type("The Technology of the Future.")
+  .pause(1000)
+  .delete(7)
+  .pause(1000)
+  .type("Past.")
+  .pause(1000)
+  .go();
+  
 
-//Handle stuff
-var applicationId = window.location.pathname.replace('/', '');
-if(applicationId == '') { applicationId = 'undefined' }
-
-
-colApplications.doc(applicationId).get().then((doc) => {
-  if(doc && doc.exists) {
-    welcome.innerText = 'Dataset found: ' + doc.data().src
-    document.body.style.background = doc.data().bg.includes('#') ? doc.data().bg : `#${doc.data().bg}`
-  } else {
-    welcome.innerText = 'Dataset not found.'
-  }
-});
