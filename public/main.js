087ec1825559c964955df06ac5ee8afdc31d06de
Maxi03milian <46729527+Maxi03milian@users.noreply.github.com>
2022-02-01 14:06:59 +0100

    contact cards animation

--- public/main.js
+++ public/main.js
@@ -130,6 +130,10 @@ const observer = new IntersectionObserver(entries => {
         entry.target.classList.add('animate__animated', 'animate__fadeInUp');
       }
 
+      if (entry.target.classList.contains('contactField')) {
+        entry.target.classList.add('animate__animated', 'animate__flipInY');
+      }
+
     }
   });
 });
@@ -143,6 +147,9 @@ observer.observe(document.querySelector('#timelineID2'));
 observer.observe(document.querySelector('#profilePicID'));
 observer.observe(document.querySelector('#skillsCodingID'));
 observer.observe(document.querySelector('#skillsLanguagesID'));
+observer.observe(document.querySelector('#teamsField'));
+observer.observe(document.querySelector('#outlookField'));
+observer.observe(document.querySelector('#intranetField'));
 
 
 
