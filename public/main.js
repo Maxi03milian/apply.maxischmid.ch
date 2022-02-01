d19ea6e1e0a7bf9be907627845a0a46a1395239b
Maxi03milian <46729527+Maxi03milian@users.noreply.github.com>
2022-02-01 14:09:09 +0100

    added delay

--- public/main.js
+++ public/main.js
@@ -131,7 +131,7 @@ const observer = new IntersectionObserver(entries => {
       }
 
       if (entry.target.classList.contains('contactField')) {
-        entry.target.classList.add('animate__animated', 'animate__flipInY');
+        entry.target.classList.add('animate__animated', 'animate__flipInY', 'animate__delay-1s');
       }
 
     }
